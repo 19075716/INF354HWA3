1e8721229aa13484fbb7f3f3b8c4fd23b0ea8d3b
19075716 <u19075716@tuks.co.za>
2023-05-27 23:48:32 +0200

    bootstrap 2

--- HWA3Angular/src/app/app.module.ts
+++ HWA3Angular/src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './login-page/login-page.component';
+//import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
