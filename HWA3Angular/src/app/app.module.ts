aef1273ca81a5c539f880ef1a8c9d251d8669b42
19075716 <u19075716@tuks.co.za>
2023-05-29 02:49:25 +0200

    latest

--- HWA3Angular/src/app/app.module.ts
+++ HWA3Angular/src/app/app.module.ts
@@ -4,17 +4,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { SidebarComponent } from './common/sidebar/sidebar.component';
+import { RegisterComponent } from './register/register.component';
+import { ProductListingComponent } from './product-listing/product-listing.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 //import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginPageComponent,
-    SidebarComponent
+    SidebarComponent,
+    RegisterComponent,
+    ProductListingComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatIconModule,
+    MatButtonModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ HWA3Angular/src/app/product-listing/product-listing.component.ts
@@ -0,0 +1,18 @@
+import { Component, AfterViewInit, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+
+@Component({
+  selector: 'app-product-listing',
+  templateUrl: './product-listing.component.html',
+  styleUrls: ['./product-listing.component.css']
+})
+export class ProductListingComponent implements OnInit, AfterViewInit{
+
+  ngOnInit(): void {
+    
+  }
+
+  ngAfterViewInit(): void {
+    
+  }
+}
