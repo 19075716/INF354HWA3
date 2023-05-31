7542945b0c16ec2011a446d5ce1fb073c80fb177
19075716 <u19075716@tuks.co.za>
2023-05-31 05:28:04 +0200

    sss

--- Assignment3_API/Controllers/SecurityController.cs
+++ Assignment3_API/Controllers/SecurityController.cs
@@ -20,9 +20,102 @@ namespace Assignment3_Backend.Controllers
     public class SecurityController : ControllerBase
     {
         private readonly UserManager<AppUser> _userManager;
-        public SecurityController(UserManager<AppUser> userManager)
+        private readonly IConfiguration _configuration;
+        public SecurityController(UserManager<AppUser> userManager, IConfiguration configuration)
         {
             _userManager = userManager;
+            _configuration = configuration;
+        }
+
+        //LP10 ClickUP
+        [HttpPost]
+        [Route("Register")]
+        public async Task<IActionResult> Register(UserViewModel uvm)
+        {
+            var user = await _userManager.FindByIdAsync(uvm.emailaddress);
+            if (user == null)
+            {
+                user = new AppUser
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    UserName = uvm.emailaddress,
+                    Email = uvm.emailaddress
+                };
+
+                var result = await _userManager.CreateAsync(user, uvm.password);
+
+                if (result.Errors.Count() > 0) return StatusCode(StatusCodes.Status500InternalServerError, "Internal Server Error. Please contact support.");
+            }
+            else
+            {
+                return Forbid("Account already exists.");
+            }
+
+            return Ok();
+        }
+
+        //Ai Generated
+        [HttpPost]
+        [Route("Login")]
+        public async Task<IActionResult> Login(UserViewModel uvm)
+        {
+            var user = await _userManager.FindByNameAsync(uvm.emailaddress);
+
+            if (user != null && await _userManager.CheckPasswordAsync(user, uvm.password))
+            {
+                try
+                {
+                    var token = GenerateJWTToken(user);
+                    return Created("", new { token, user = user.UserName });
+                }
+                catch (Exception)
+                {
+                    return StatusCode(StatusCodes.Status500InternalServerError, "Internal Server Error. Please contact support.");
+                }
+            }
+            else
+            {
+                return NotFound("Does not exist");
+            }
+        }
+
+        //Ai Generated
+        [HttpGet]
+        private string GenerateJWTToken(AppUser user)
+        {
+            var claims = new List<Claim>
+         {
+        new Claim(ClaimTypes.NameIdentifier, user.Id),
+        new Claim(ClaimTypes.Name, user.UserName),
+          };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Tokens:Key"]));
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddHours(3),
+                SigningCredentials = credentials,
+                Issuer = _configuration["Tokens:Issuer"],
+                Audience = _configuration["Tokens:Audience"]
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
+
+        //Ai Generated
+        [HttpPost]
+        [Route("Logout")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync();
+
+            return Ok();
         }
     }
 }
